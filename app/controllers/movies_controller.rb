618e824738ada682db2c6093408139f378763fd5
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-11-21 15:47:14 -0300

    add test to movies#show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,9 @@
 class MoviesController < ApplicationController
-  def index; end
+  def index
+    @movies = Movie.all
+  end
+
+	def show
+		@movie = Movie.find(params[:id])
+	end
 end
--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -1,9 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Movies', type: :request do
+  let(:valid_attributes) do
+    {
+      title: 'A Article Title',
+      release_year: Time.zone.now - 1.year
+    }
+  end
+
   describe 'GET /index' do
     it 'return a successful response' do
       get '/movies/'
+
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'return a successful response' do
+      movie = Movie.create! valid_attributes
+      get movie_url(movie)
 
       expect(response).to have_http_status(:ok)
     end
