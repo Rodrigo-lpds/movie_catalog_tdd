c2152318fc071b5087e1727b0f2961cc2530d576
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-11-21 15:31:57 -0300

    pass test of movie#index

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Movies', type: :request do
   describe 'GET /index' do
-    it 'list all movies' do
+    it 'return a successful response' do
       get '/movies/'
 
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
