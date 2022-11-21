2efbc5e8487f53187c91cb79cf7b36041a1db4fa
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-11-21 11:31:05 -0300

    validates the presence of title and release year attributes of movie

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  describe 'Valid Model' do
-    it 'create a new Movie' do
-      movie = Movie.create(title: 'Movie Title', release_year: Time.zone.now.year)
-      expect(movie).to be_valid
+  describe 'Validations' do
+    it 'with invalid title' do
+      expect(Movie.create(release_year: Time.zone.now.year)).to_not be_valid
+    end
+    it "release year cant't be blank" do
+      expect(Movie.create(title: 'Movie title')).to_not be_valid
     end
   end
 end
