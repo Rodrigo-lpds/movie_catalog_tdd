27acb3f413d3db6a6203c6d52453dd5e1d7dbc84
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-11-21 11:15:24 -0300

    add test of happy path when creating the movie model

--- db/migrate/20221121140009_create_movies.rb
+++ db/migrate/20221121140009_create_movies.rb
@@ -2,7 +2,7 @@ class CreateMovies < ActiveRecord::Migration[7.0]
   def change
     create_table :movies do |t|
       t.string :title
-      t.datetime :realease_year
+      t.datetime :release_year
 
       t.timestamps
     end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Valid Model' do
+    it 'create a new Movie' do
+      movie = Movie.create(title: 'Movie Title', release_year: Time.zone.now.year)
+      expect(movie).to be_valid
+    end
+  end
 end
